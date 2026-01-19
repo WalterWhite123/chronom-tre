539211309e18aae399c89845a5e36bd7d0ff5cc1
ManWithDreams <103361329+WalterWhite123@users.noreply.github.com>
2026-01-19 19:19:28 +0100

    chrono almost done

---
+++ chrono.js
@@ -0,0 +1,52 @@
+let heure = 0;
+let minute = 0;
+let secondes = 0;
+let millisecondes = 0;
+let display = document.querySelector("#display");
+
+
+
+
+let on = true;
+const start = document.querySelector("#start");
+const pause = document.querySelector("#pause");
+start.addEventListener("click",()=> {
+   
+  
+        
+        let chronometre = setInterval(function(){
+                
+        millisecondes++;
+    display.textContent = `${heure}:${secondes}:${millisecondes}`;
+    if (millisecondes >= 1000){
+        millisecondes = 0;
+        secondes += 1;
+    }
+    if (secondes >= 60){
+        secondes = 0;
+        minute += 1;
+    }
+    if (minute >= 60){
+        minute = 0;
+        heure += 1;
+    }
+   
+
+   pause.addEventListener("click",()=>{
+    clearInterval(chronometre);
+   })      
+
+    
+},1)
+
+
+
+
+   
+    
+
+
+
+
+})
+
